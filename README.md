97e9a10527c2927fa12ba9f2ab5b455f766f3e63
刘宝权 <liubq@jcinfo.online>
2020-06-02 09:38:25 +0800

    V1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+#非原创，使用了https://github.com/Radom7/springboot-layui.git这个工程代码
+#但是不喜欢用Lombok这个工具，也加了一点自己的代码就自己重新传了一份，主要是学习用
+#下面是原来的介绍
+# SpringBoot+Layui后台管理系统
+## 后端：
+SpringBoot  2.1.0  
+tk.mybatis  2.0.2  
+Shiro  1.3.1  
+Lombok 1.18.4  
+## 前端：
+Layui  2.4.5  
+JQuery  3.3.1
+
+## 渲染模板
+Thymeleaf
---
+++ src/main/java/com/haiyu/manager/dto/AdminRoleDTO.java
@@ -0,0 +1,110 @@
+package com.haiyu.manager.dto;
+
+/**
+ * @Title: AdminRoleDTO
+ * @Description:
+ * @author: youqing
+ * @version: 1.0
+ * @date: 2018/12/3 18:51
+ */
+public class AdminRoleDTO {
+    private Integer id;
+
+    /**
+     * 角色名称
+     */
+    private String roleName;
+
+    /**
+     * 角色描述
+     */
+    private String roleDesc;
+
+    private String permissionIds;
+    /**
+     * 权限
+     */
+    private String permissions;
+
+    /**
+     * 创建时间
+     */
+    private String createTime;
+
+    /**
+     * 更新时间
+     */
+    private String updateTime;
+
+    /**
+     * 1：有效
+     0：无效
+     */
+    private Integer roleStatus;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getRoleDesc() {
+		return roleDesc;
+	}
+
+	public void setRoleDesc(String roleDesc) {
+		this.roleDesc = roleDesc;
+	}
+
+	public String getPermissionIds() {
+		return permissionIds;
+	}
+
+	public void setPermissionIds(String permissionIds) {
+		this.permissionIds = permissionIds;
+	}
+
+	public String getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(String permissions) {
+		this.permissions = permissions;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public Integer getRoleStatus() {
+		return roleStatus;
+	}
+
+	public void setRoleStatus(Integer roleStatus) {
+		this.roleStatus = roleStatus;
+	}
+    
+    
+}
